a31994b17356c83f535a3580887ce9a4e6c69991
leoneleg <leoneleg@ext.inditex.com>
2023-10-09 10:06:14 +0200

    fix logger

--- src/main/java/org/labruzeza/colectividades/GargareCollection.java
+++ src/main/java/org/labruzeza/colectividades/GargareCollection.java
@@ -1,10 +1,10 @@
 package org.labruzeza.colectividades;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 
 public class GargareCollection extends Thread {
-	private static final Logger logger = LogManager.getLogger(GargareCollection.class);
+	private static final Logger logger = LoggerFactory.getLogger(GargareCollection.class);
 	
 	
 	@SuppressWarnings("static-access")
